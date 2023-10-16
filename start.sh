ea3d6715fac01e1ef5018675a5e33af077927299
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-10-16 17:00:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/htmlboss123/YT-FLITER/
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /YT-FLITER
 fi
-cd /Ajax
+cd /YT-FLITER
 pip3 install -U -r requirements.txt
 echo "Starting ᗩᒍᗩ᙭....🔥"
 python3 bot.py
